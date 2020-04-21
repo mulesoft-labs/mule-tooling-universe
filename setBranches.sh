b70cef7512c80d231db8651ac4d0c51c231bdb97
Martin D'Aloia <mdaloia@mulesoft.com>
2020-04-21 13:13:40 -0300

    Small improvement to setBranches.sh script

--- setBranches.sh
+++ setBranches.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
 
-# echo "$(ls .git/modules)"
-
-# grep -e "url" .gitmodules -C 2 | sed -e 's/^[ 	]*//'
-
 BRED='\033[1;31m'
 BGREEN='\033[1;32m'
-IWhite='\033[0;97m'
 NC='\033[0m'
 
 paths=($(grep -e "path" .gitmodules | sed -e 's/path =//'))
 branches=($(grep -e "branch" .gitmodules | sed -e 's/branch =//'))
+urls=($(grep -e "url" .gitmodules | sed -e 's/url =//' -e 's/git@github\.com:/https:\/\/github.com\//' -e 's/\.git//'))
 numOfModules=$(grep -e "branch" .gitmodules | sed -n '$=')
 length=${#branches[@]}
 
-# echo ${branches[3]}
-
 for ((i = 0; i != length; i++)); do
-	echo "${BGREEN}Path: '${paths[i]}' -- Branch: '${branches[i]}'${NC}"
+	echo "${BGREEN}Path: ${NC}${paths[i]}${BRED} -- ${BGREEN}Branch: ${NC}${branches[i]}${BRED} -- ${BGREEN}URL: ${NC}${urls[i]}/tree/${branches[i]}"
 
 	(cd ${paths[i]} && git checkout ${branches[i]})
 done
